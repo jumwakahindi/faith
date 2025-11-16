f913d50f47ff5450a2a3358caf8f8d0fd6f6f6ea
Faith <kahindifaith69@gmail.com>
2025-11-15 18:35:32 -0800

    datatypes and operations

---
+++ Python development/Operators and data types/Main.py
@@ -0,0 +1,17 @@
+name="faith"
+grade=12
+Age=17
+Weight=51
+Is_student=True#Display student information
+print("STUDENT DETAILS")
+print(f"name:{name}")
+print(f"grade:{grade}")
+print(f"Age:{Age}")
+print(f"Weight:{Weight}")
+print(f"Is this a student:{Is_student}")
+#datatypes of the variables
+print("The datatype of name is:",type(name))
+print("The datatype of grade is:",type(grade))
+print("The datatype of Age is:",type(Age))
+print("The datatype of  Weight is:",type(Weight))
+print("The datatype of Is_student is:",type(Is_student))
---
+++ Python development/Operators and data types/Main1.py
@@ -0,0 +1,17 @@
+#ask the user to enter 2 numbers
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+#Arithmetic operations
+addition=num1+num2
+mult=num1*num2
+div=num1/num2
+sub=num1-num2
+fd=num1//num2
+md=num1%num2
+#print the results
+print("addition:",addition)
+print("subtraction:",sub)
+print("multiplication:",mult)
+print("division:",div)
+print("floor division:",fd)
+print("modulus:",md)

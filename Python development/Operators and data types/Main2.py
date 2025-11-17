9f88fb44f5e504fe0d9cd1e5f52780ef960d2f34
Faith <kahindifaith69@gmail.com>
2025-11-16 17:23:04 -0800

    swapping

--- Python development/Operators and data types/Main2.py
+++ Python development/Operators and data types/Main2.py
@@ -1,7 +1,7 @@
 #Asking the user to enter numbers
 num1=int(input("Enter a number:"))
 num2=int(input("Enter another number:"))
-#Asking the user to swap the numbers(pythonic way)
+#Asking the user to swap the numbers(using pythonic way)
 a=10
 b=20
 a,b= b,a
@@ -13,7 +13,7 @@ temp=a
 a=b
 b=temp
 print(a, b)
-#Asking the user to swap numbers(arithmetic)
+#Asking the user to swap numbers(using arithmetic)
 a=9
 b=8
 a=a+b
